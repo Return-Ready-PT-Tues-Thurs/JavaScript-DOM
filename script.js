1afd305a6a139b89db60638a3d7e6a2136d65d52
Kimberly Ramirez <kimberly.ramirez0313@gmail.com>
2020-12-29 19:20:21 -0500

    add image

---
+++ script.js
@@ -0,0 +1,8 @@
+let message = document.querySelectorAll('p');
+for (let i = 0; i < message.length; i++){
+    message[i].innerHTML = "We are coders!";
+}
+document.querySelector('#one').innerHTML = "Developers for life!" ;
+let image = document.createElement('img');
+image.src  = 'kitten.jpg';
+document.body.appendChild(image);
