248e3baba3da652cdcea810f5832d96bd4ece080
Kimberly Ramirez <kimberly.ramirez0313@gmail.com>
2020-12-29 20:36:22 -0500

    add color

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let name = document.querySelector('#name');
 let fave = document.querySelector('#fave');
 let home = document.querySelector("#home");
 let fact = document.querySelector('#fact');
+let listColor = document.querySelectorAll('li');
 
 document.querySelector("body").style.fontFamily = "Arial, sans-serif";
 
@@ -9,3 +10,7 @@ name.innerHTML = "Kim";
 fave.innerHTML = "food, anime";
 home.innerHTML = "Philippines";
 fact.innerHTML = "I have an annoying cat";
+
+for( let i=0; i < listColor.length; i++){
+    listColor[i].style.color = "lightblue";
+}
