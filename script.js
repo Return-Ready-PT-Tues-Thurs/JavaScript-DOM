0da4dfbbd8bf3b917fb2be9714d819f3db58726c
Kimberly Ramirez <kimberly.ramirez0313@gmail.com>
2020-12-29 19:59:55 -0500

    part1 finish

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 let message = document.querySelectorAll('p');
+let p1 = document.querySelector('#one');
+let image = document.createElement('img');
+const button = document.createElement('button');
+
+
 for (let i = 0; i < message.length; i++){
     message[i].innerHTML = "We are coders!";
+    message[i].style.color = "steelblue";
 }
-document.querySelector('#one').innerHTML = "Developers for life!" ;
-let image = document.createElement('img');
+
+p1.innerHTML = "Developers for life!" ;
+
 image.src  = 'kitten.jpg';
 document.body.appendChild(image);
+
+p1.style.fontSize = "40px";
+
+button.innerHTML = "Hide Me!";                   
+document.body.appendChild(button);  
+
+
+document.querySelector('button').addEventListener("click", clickMe);
+function clickMe() {
+    var x = document.querySelector('img');
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      button.innerHTML = "Hide Me!";
+    } else {
+      x.style.display = "none";
+      button.innerHTML = "Show Me!";
+    }
+  }
