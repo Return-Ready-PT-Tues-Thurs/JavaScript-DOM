359aa0c00624b548371eed377dbce40e69d4d3d0
koggenfuss <koggenfuss@gmail.comllsgit config --global user.email koggenfuss@gmail.com>
2021-01-04 22:48:55 -0500

    added comments, fixed button bug, image bug

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 window.addEventListener("load", (event) => {
     /*Style font on window load*/
     document.querySelector("body").style.fontFamily = "Arial, sans-serif";
+    
     /*Add image on window load*/
     let image = document.createElement("img");
     image.src = "images/ratio3x2_1200.jpg";
     document.body.appendChild(image);
-    /*create image on window load*/
+    
+    
+    /*Create image on window load*/
     let picOfMe = document.createElement("img");
-    image.src = "images/IMG_0285.JPG";
-    document.body.appendChild(picOfMe);
+    picOfMe.src = "images/IMG_0285.JPG";
+    document.body.append(picOfMe);
 });
 
-
+/*Populate facts about me in existing unordered list*/
 let factsAboutMe = ["Nickname: Kiki", "Favorites: jazz, piano, Halloween", "Hometown: Middletown", "Random Fact: I started playing the piano at 4 years old"];
 let li = document.querySelectorAll("li"); 
 for(i=0;i<factsAboutMe.length;i++){
