d6ca2d2a9a71658718b9ae5ae507c264269f21a5
coopcool <coopcool78@gmail.com>
2021-01-13 21:42:54 -0500

    DOM ProJects

---
+++ main.js
@@ -0,0 +1,8 @@
+
+        function myFunction() {
+          document.getElementById("nickname").style.fontFamily=Arial;
+          document.getElementById("favorites").style.fontFamily;
+          document.getElementById("hometown").style.fontFamily;
+          document.getElementById("random-fact").style.fontFamily;
+        }
+   
