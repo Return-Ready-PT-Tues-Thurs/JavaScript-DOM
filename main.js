1c1ed3a06ca9730f1be4e75d9d9b478f20cc4e2e
Kayanr <kayan.reid@gmail.com>
2021-01-18 15:56:35 -0500

    add main.js and ids to <span> in about.html

---
+++ main.js
@@ -0,0 +1,10 @@
+// Change the body style so it has a `font-family` of `Arial, sans-serif`.
+document.querySelector("body").style.fontFamily = "Arial, sans-serif";
+
+//Replace each of the values with your own information.
+document.getElementById("nickname").textContent = "Nickname: cayenne pepper";
+document.getElementById("favorites").textContent =
+  "Favorites: music, cheesecake, The Office";
+document.getElementById("hometown").textContent = "Hometown: New Castle";
+document.getElementById("randomFact").textContent =
+  "Random Fact: Red is my favorite color.";
